2c33c1fe003eb593fef7eae48fb210dedd48881b
catarina6t <catarinatorres@live.com.pt>
2021-10-26 16:01:43 +0100

    Added some Components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import Search from "./Search";
 
-export default function Weather() {
+function App() {
   return (
     <div className="App">
       <h1 />
-      <App />;
-      <reportWebVitals />
+      <Search />;
     </div>
   );
 }
